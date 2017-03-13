cbb2590fceaea7cdb1a9ce7bf930c07320be072c
Iván Corrales Solera <developer@wesovilabs.com>
2017-03-13 05:24:51 +0100

    feat: updating formula

--- gherkinize.rb
+++ gherkinize.rb
@@ -1,11 +1,9 @@
 class Gherkinize < Formula
   desc "A command line tool to check out gherkin scenarioes are correctly writen"
   homepage "https://github.com/wesovilabs/gherkinize"
-  url "https://github.com/TheHipbot/weather/raw/master/archive/weather-1.0.0.tar.gz"
-  sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
-  version "1.0.0"
-
-  depends_on "curl"
+  url "https://github.com/wesovilabs/gherkinize/archive/v0.0.1-rc1.tar.gz"
+  sha256 "c4ee14840603a77315791e9404104ff30bd9b88cd227f7814a47b763bcfdf624"
+  version "0.0.1-rc1"
 
   bottle :uneeded
 
