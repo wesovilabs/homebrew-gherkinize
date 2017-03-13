89f48623a2ad98c3d436a376b0f73c672196a83f
Iván Corrales Solera <developer@wesovilabs.com>
2017-03-13 06:35:16 +0100

    eat: updating formula

--- gherkinize.rb
+++ gherkinize.rb
@@ -1,9 +1,9 @@
 class Gherkinize < Formula
   desc "A command line tool to check out gherkin scenarioes are correctly writen"
   homepage "https://github.com/wesovilabs/gherkinize"
-  url "https://github.com/wesovilabs/gherkinize/archive/v0.0.1-rc1.tar.gz"
-  sha256 "c4ee14840603a77315791e9404104ff30bd9b88cd227f7814a47b763bcfdf624"
-  version "0.0.1-rc1"
+  url "https://github.com/wesovilabs/gherkinize/archive/0.0.1-rc2.tar.gz"
+  sha256 "990be52f59a838fe9ebee475208e0c3de60662b7e55d2560b53c1b3ab3df60a0"
+  version "0.0.1-rc2"
 
   bottle :uneeded
 
